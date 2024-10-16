edf9f9bbdf6a4a6398bf6d9c7a4c99137aadc164
Arnaud-C18 <clavier.a18@gmail.com>
2024-10-16 16:44:44 +0200

    Initialisation du projet

---
+++ src/main/java/ArnaudC18/App.java
@@ -0,0 +1,13 @@
+package ArnaudC18;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
